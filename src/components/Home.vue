094531e265b7ee7437dd3b78573596f1eb570906
vinylen2 <gabrielwallen93@gmail.com>
2020-05-04 19:49:35 +0200

    add image window

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,11 +4,43 @@
     <v-col align="center" class="headline">
       <span>Startsida</span>
     </v-col>
+    <v-btn
+      @click="uploadDialog = true"
+      bottom
+      right
+      fixed
+      fab>
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <v-dialog v-model="uploadDialog" persistent>
+      <UploadImage 
+        @closeDialog="closeDialog"/>
+    </v-dialog>
   </v-row>
 </v-content>
 </template>
 
 <script>
+import UploadImage from '@/components/Image/UploadImage';
+
 export default {
+  name: 'upload-image',
+  components: {
+    UploadImage,
+  },
+  data: () => ({
+    uploadDialog: false,
+  }),
+  methods: {
+    closeDialog(data) {
+      if (data) {
+        // stuff was uploaded
+      } else {
+        // stuff was not uploaded
+      }
+      this.uploadDialog = false;
+    },
+
+  },
 };
 </script>
---
+++ src/components/Image/UploadImage.vue
@@ -0,0 +1,81 @@
+<template>
+<v-card>
+  <v-card-title>
+    <span class="headline">
+      Ladda upp bild
+    </span>
+    </v-card-title>
+  <v-card-text>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-file-input
+              label="File input"
+              filled
+              v-model="image"
+              prepend-icon="mdi-camera">
+            </v-file-input>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6">
+            <v-text-field label="Förnamn" 
+              required 
+              :rules="rules"
+              v-model="firstname">
+            </v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field label="Efternamn" 
+              required 
+              :rules="rules"
+              v-model="lastname">
+            </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-btn color="red darken-1" text @click="closeDialog">Stäng</v-btn>
+          <v-btn color="blue darken-1" text @click="uploadImage">Spara</v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-card-text>
+</v-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'upload-image',
+  data: () => ({
+    valid: false,
+    rules: [
+      v => !!v || 'Fältet måste fyllas i',
+    ],
+    firstname: '',
+    lastname: '',
+    image: '',
+  }),
+  computed: {
+    ...mapGetters([
+      'token',
+    ]),
+  },
+  methods: {
+    /* eslint-disable no-console */
+    closeDialog() {
+      this.resetFields();
+      this.$emit('closeDialog');
+    },
+    resetFields() {
+      this.firstname = '';
+      this.lastname = '';
+    },
+    uploadImage() {
+
+    },
+  },
+};
+</script>
