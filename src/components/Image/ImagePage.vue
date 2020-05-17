387fd30e252b3b0b76942e2909b156a1e132f0a3
lilGandhi <50099733+lilGandhi@users.noreply.github.com>
2020-05-17 18:05:39 +0200

    allt

--- src/components/Image/ImagePage.vue
+++ src/components/Image/ImagePage.vue
@@ -8,41 +8,41 @@
 
     <v-card-text>
 
-      <v-row>
+      <v-row v-if="info.photographer.name !== null">
          <div>Fotograf:</div><div class="text--primary"> {{info.photographer.name}}</div>
    </v-row>
       
-   <v-row>
-         <div>Beskrivning:</div><div class="text--primary">{{info.description}}</div>
+   <v-row v-if="info.description !== null">
+         <div>'Beskrivning:' +{{space}}</div><div class="text--primary">{{info.description}}</div>
    </v-row>
-   <v-row>
-         <div>Skapad:</div><div class="text--primary"> {{author}}</div>
+   <v-row v-if="authorDate!== null">
+         <div>Skapad: </div><div class="text--primary"> {{authorDate}}</div>
    </v-row>
-   <v-row>
+   <v-row v-if="info.gpsLongitude !== null">
          <div>Gps latitud: </div><div class="text--primary"> {{info.gpsLatitude}}</div>
    </v-row>
-   <v-row>
+   <v-row v-if="info.gpsLongitude !== null"> 
          <div>Gps longitud :</div><div class="text--primary"> {{info.gpsLongitude}}</div>
    </v-row>
-   <v-row>
-         <div>Kameramodell:</div><div class="text--primary"> {{info.cameraModel}}</div>
+   <v-row v-if="info.cameraModel !== null">
+         <div >Kameramodell:</div><div class="text--primary"  > {{info.cameraModel}}</div>
    </v-row>
-   <v-row>
+   <v-row v-if="info.imageWidth !== null">
          <div>Bildbredd:</div><div class="text--primary"> {{info.imageWidth}}</div>
    </v-row>
-   <v-row>
+   <v-row v-if="info.gpsLongitude !== null">
          <div>Bildhöjd:</div><div class="text--primary"> {{info.imageHeight}}</div>
    </v-row>
-   <v-row>
+   <v-row v-if="info.captured !== null">
          <div>Fångad:</div><div class="text--primary"> {{info.captured}}</div>
    </v-row>
-    <v-row>
+    <v-row v-if="info.resolution !== null">
          <div>Resolution:</div><div class="text--primary"> {{info.resolution}}</div>
    </v-row>
-   <v-row>
+   <v-row v-if="info.license !== null">
          <div>Licens:</div><div class="text--primary"> {{info.license}}</div>
    </v-row>
-   <v-row>
+   <v-row v-if="info.usesLeft !== null">
          <div>Användningar kvar:</div><div class="text--primary"> {{info.usesLeft}}</div>
    </v-row>
   
@@ -82,6 +82,7 @@ computed: {
     return {
       id: this.$route.params.id,
       info: {},
+      space: " ",
   
     };
   },
